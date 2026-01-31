3ddde9fb715d835189b582e91d134248d21835b7
yixuan228 <yixuan_li0226@163.com>
2026-01-31 15:31:11 +0800

    修复了总饲喂量是两倍的问题

--- src/data_etl/utils/aggregation.py
+++ src/data_etl/utils/aggregation.py
@@ -1,9 +1,11 @@
 # 返回不同精度等级的数据
 def split_granularity(df):
-    df_col = df[['Date', 'col_num', 'food_col_kg']].drop_duplicates()
+    df_col = df[['Date', 'col_num', 'food_col_kg']].drop_duplicates(subset=['Date', 'col_num', 'food_col_kg'])
     df_col = df_col.sort_values(by=['Date', 'col_num'], ascending=[True, True])
 
-    df_build = df[['Date', 'food_total_kg']].drop_duplicates()
+    df_build = df[['Date', 'food_total_kg']].drop_duplicates(subset=['Date', 'food_total_kg'])
     df_build = df_build.sort_values(by=['Date'], ascending=True)
+
+    # print(df_build[df_build['food_total_kg']>100, :]) # debug
 
     return df_col, df_build
